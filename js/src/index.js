4a906d121bb53d07441cdd577a5fe60fbe40e3cb
Leon Thomm <l.thomm@mailbox.org>
2025-02-06 14:03:12 +0000

    fixup

--- js/src/index.js
+++ js/src/index.js
@@ -1,12 +1,9 @@
 import { $init, id } from './my-component/my-component';
-// import { $init } from './builder/builder';
 
 $init().then(() => {
   console.log('init');
 });
 
-// TODO: why does this work, while component-studio2 does not?
-
-export function get(d) {
+export function f(d) {
   return id(d);
 }
